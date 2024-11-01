33af5f3d77820eb8f2c0926bd8a133ce7601ba57
Sahithyan <sahithyan2701@gmail.com>
2024-11-02 00:37:17 +0530

    feat: add new script for generating pdfs

---
+++ scripts/generate-pdfs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd src/content/docs
+files=$(find "summary" -name "*.md")
+echo $files
+
+mkdir -p ../../../public/as-pdf
+cd ../../../public/as-pdf
+
+command_args=""
+
+for file in $files; do
+    filename="${file/.md//}"
+    filename="${filename/\.\//}"
+
+    page_url="http://localhost:4321/$filename"
+
+    command_args+=" --inputs $page_url"
+done
+
+html-export-pdf-cli $command_args
